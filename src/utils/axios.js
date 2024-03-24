ea06c25b464ddea2036c6d5edf00ffd240ff9945
vishal <jakharvishalv@gmail.com>
2024-03-24 15:54:01 +0530

    deploy link fix

--- src/utils/axios.js
+++ src/utils/axios.js
@@ -3,7 +3,7 @@ import Axios from "axios";
 const isInProduction = import.meta.env.PROD;
 const baseAPIPath = import.meta.env.BASE_URL;
 
-const HOST = isInProduction ? "" : "http://localhost:5100";
+const HOST = isInProduction ? "https://invoicemanagement-backend.onrender.com" : "http://localhost:5100";
 
 const axios = Axios.create({
 	baseURL: `${HOST}${baseAPIPath}`,
