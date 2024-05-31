317489b0fe094ccd09232a8846ec49887a482a59
vishal <jakharvishalv@gmail.com>
2024-05-31 12:08:19 +0530

    aws deploy

--- src/utils/axios.js
+++ src/utils/axios.js
@@ -3,7 +3,7 @@ import Axios from "axios";
 const isInProduction = import.meta.env.PROD;
 const baseAPIPath = import.meta.env.BASE_URL;
 
-const HOST = isInProduction ? "https://invoicemanagement-backend.onrender.com" : "http://localhost:5100";
+const HOST = !isInProduction ? "http://52.66.117.99:8001" : "http://localhost:5100";
 
 const axios = Axios.create({
 	baseURL: `${HOST}${baseAPIPath}`,
